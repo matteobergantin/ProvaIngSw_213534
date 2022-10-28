c7db9b2b7e92c35a2cb5043c50995c8a5aa4a03a
matteobergantin <matteobergantin161@gmail.com>
2022-10-28 15:58:24 +0200

    Added basic tests

--- ingsw/src/main/java/it/unical/mat/tests/StringToIntTests.java
+++ ingsw/src/main/java/it/unical/mat/tests/StringToIntTests.java
@@ -1,5 +1,6 @@
 package it.unical.mat.tests;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.*;
 import it.unical.mat.ingsw.FunnyAlgorithms;
 
@@ -15,6 +16,22 @@ public class StringToIntTests {
 	public static void cleanup() {
 		_instance = null;
 	}
-	
 
+	@Test (expected = IllegalArgumentException.class)
+	public void floatNumbersNotAllowed() {
+		_instance.stringToIntConverter("11.23");
+	}
+	
+	@Test
+	public void negativeNumbersWork() {
+		assertEquals(_instance.stringToIntConverter("-12"), -12);
+		assertEquals(_instance.stringToIntConverter("-123"), -123);
+		assertEquals(_instance.stringToIntConverter("-334"), -334);
+		assertEquals(_instance.stringToIntConverter("-156"), -156);
+	}
+
+	@Test (expected = NumberFormatException.class)
+	public void numberInRange() {
+		_instance.stringToIntConverter("1000000");
+	}
 }
